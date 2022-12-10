350b06a5f69872eb692bfc341af8b6aed1a7b3a4
Alyeldeen66 <alyeldeenkhaled2@gmail.com>
2022-12-10 23:58:21 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import NavBar from "./Components/navbar/NavBar";
-
+import Home from "./Components/Home/Home.js";
 function App() {
   return (
     <div className="main-app">
       <NavBar />
+      <Home />
     </div>
   );
 }
