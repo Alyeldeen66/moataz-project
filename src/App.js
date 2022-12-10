10ad18604a226694e8b24ef087719f436104c8f1
Alyeldeen66 <alyeldeenkhaled2@gmail.com>
2022-12-10 22:55:28 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./Components/navbar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
+    <div className="main-app">
+      <NavBar />
     </div>
   );
 }
--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -2,8 +2,25 @@ import React from "react";
 import "./NavBar.css";
 const NavBar = () => {
   return (
-    <div>
-      <div></div>
+    <div className="nav-container">
+      <div className="main-nav">
+        <div className="nav-image">
+          <img src="https://pixlr.com/images/index/remove-bg.webp" />
+        </div>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
