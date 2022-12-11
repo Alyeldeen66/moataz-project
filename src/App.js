5731fb3a46496781ac550ff73ece9668d79bf17f
Alyeldeen66 <alyeldeenkhaled2@gmail.com>
2022-12-11 21:31:38 +0200

    updated some media queries for the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./Components/navbar/NavBar";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="main-app">
       <NavBar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="main-footer">
+        <h2>hola</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
