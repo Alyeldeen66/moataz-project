ce67a41b23f5d7cb0dae6c501eaa94f581008277
Alyeldeen66 <alyeldeenkhaled2@gmail.com>
2022-12-11 22:57:58 +0200

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,111 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="main-footer">
-        <h2>hola</h2>
+        <div className="footer-item">
+          <div>
+            <h3>Home</h3>
+          </div>
+          <div className="footer-list-container">
+            <ul className="footer-list">
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+            </ul>
+          </div>
+          <hr className="break" />
+        </div>
+        <div className="footer-item">
+          <div>
+            <h3>About</h3>
+          </div>
+          <div className="footer-list-container">
+            <ul className="footer-list">
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+            </ul>
+          </div>
+          <hr className="break" />
+        </div>
+        <div className="footer-item">
+          <div>
+            <h3>Tools</h3>
+          </div>
+          <div className="footer-list-container">
+            <ul className="footer-list">
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+            </ul>
+          </div>
+          <hr className="break" />
+        </div>
+        <div className="footer-item">
+          <div>
+            <h3>My Account</h3>
+          </div>
+          <div className="footer-list-container">
+            <ul className="footer-list">
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <a href="#">Our Pricing</a>
+                </li>
+              </div>
+            </ul>
+          </div>
+          <hr className="break" />
+        </div>
+      </div>
+      <div>
+        <div className="footer-trademark">
+          <h5>Copyright © 2022 · REI Call Center · Design by</h5>
+        </div>
       </div>
     </div>
   );
