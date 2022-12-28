1fcf7a624f8b884e15964836d6a37d4554f301dd
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-28 23:35:39 +0200

    home-page-data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,9 +11,11 @@ function Home() {
           <div className="home-title">
             <img src={logo} alt="logo" />
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-            Quisquam voluptates, quod, quia, voluptate quae voluptatem
+          <p style={{ textAlign: "center" }}>
+            Phone Pros mission is to create an opportunity for Wholesallers to
+            tackle their growth & profitability without the loss of quality or
+            standards with the lowest cost per acquisition and highest return on
+            investment.
           </p>
           <div className="home-btns">
             <button className="home-btn">GET STARTED</button>
@@ -28,39 +30,33 @@ function Home() {
           <div className="why-us-cards">
             <div className="why-us-card">
               <i className="fas fa-check-circle"></i>
-              <h2 className="why-us-card-title">Card Title</h2>
-              <p className="why-us-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+              <p className="why-us-card-title">
+                {" "}
+                We are a call center especialized in real industry
               </p>
+              <p className="why-us-card-text"></p>
             </div>
             <div className="why-us-card">
               <i className="fas fa-check-circle"></i>
-              <h2 className="why-us-card-title">Card Title</h2>
-              <p className="why-us-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+              <p className="why-us-card-title">
+                We have years of experience in the field
               </p>
+              <p className="why-us-card-text"></p>
             </div>
             <div className="why-us-card">
               <i className="fas fa-check-circle"></i>
-              <h2 className="why-us-card-title">Card Title</h2>
-              <p className="why-us-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+              <p className="why-us-card-title">
+                Starting from generating Wholsalling leads, Leads management
               </p>
+              <p className="why-us-card-text"></p>
             </div>
             <div className="why-us-card">
               <i className="fas fa-check-circle"></i>
-              <h2 className="why-us-card-title">Card Title</h2>
-              <p className="why-us-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+              <p className="why-us-card-title">
+                Leads Quality control, Negotiate pricing, Calulating ARV, and
+                getting the lead under contract.
               </p>
+              <p className="why-us-card-text"></p>
             </div>
           </div>
         </div>
@@ -80,12 +76,8 @@ function Home() {
             <div className="how-it-works-card hiw-1">
               <i className="fa-solid fa-arrow-pointer"></i>
 
-              <h3 className="how-it-works-card-title">Card Title</h3>
-              <p className="how-it-works-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
-              </p>
+              <h3 className="how-it-works-card-title">Press on get started</h3>
+              <p className="how-it-works-card-text"></p>
             </div>
             <div className="how-it-works-animation anim1">
               <div className="how-it-works-animation-circle"></div>
@@ -97,11 +89,9 @@ function Home() {
             <div className="how-it-works-card hiw-2">
               <i className="fa-solid fa-cart-shopping"></i>
 
-              <h3 className="how-it-works-card-title">Card Title</h3>
+              <h3 className="how-it-works-card-title">Choose your package</h3>
               <p className="how-it-works-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+                Choose one of our premium packages avaliable
               </p>
             </div>
             <div className="how-it-works-animation anim2">
@@ -114,11 +104,9 @@ function Home() {
             <div className="how-it-works-card hiw-3">
               <i className="fa-solid fa-credit-card"></i>
 
-              <h3 className="how-it-works-card-title">Card Title</h3>
+              <h3 className="how-it-works-card-title">Seal the deal !</h3>
               <p className="how-it-works-card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptates, quod, quia, voluptate quae voluptatem
-                quibusdam voluptas quidem quos nemo quas. Quisquam, quae.
+                Contact us through contact us section
               </p>
             </div>
             <div className="how-it-works-animation anim3">
